36a65703043045aef712f6db7cf67f32acd41d22
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-04-22 20:18:32 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -8,7 +8,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 UPDATES_CHANNEL = C.UPDATES_CHANNEL
 logging.basicConfig(level=logging.INFO)
 
-@Client.on_message(filters.incoming & filters.command(['start']) & filters.private)
+@Client.on_message(filters.incoming & filters.command(['tart']) & filters.private)
 async def _start(client, message):
     update_channel = UPDATES_CHANNEL
     if update_channel:
